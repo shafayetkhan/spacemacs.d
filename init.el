1c6e95f4d25a48b59ff4e2e03632a93246154a38
Shafayet Khan <shafayetkhan@gmail.com>
2016-06-30 20:31:05 -0600

    Add ibuffer-vc config from Sanityinc

--- init.el
+++ init.el
@@ -48,6 +48,8 @@ values."
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '(helm-projectile
+                                      ibuffer-vc
+                                      fullframe
                                       helm-ag
                                       semantic
                                       hlinum
@@ -328,8 +330,51 @@ you should place your code here."
  (global-set-key "\C-c_" 'split-window-vertically-instead)
  ;; Set keybinding to dedicate current window
  (global-set-key (kbd "C-c <down>") 'spacemacs/toggle-current-window-dedication)
- )
 
+ ;; ibuffer config from Sanityinc - this should really be in it's own layer
+ (defun ibuffer-set-up-preferred-filters ()
+   (ibuffer-vc-set-filter-groups-by-vc-root)
+   (unless (eq ibuffer-sorting-mode 'filename/process)
+     (ibuffer-do-sort-by-filename/process)))
+ (add-hook 'ibuffer-hook 'ibuffer-set-up-preferred-filters)
+ (setq-default ibuffer-show-empty-filter-groups nil)
+ (with-eval-after-load 'ibuffer
+             ;; Use human readable Size column instead of original one
+             (define-ibuffer-column size-h
+               (:name "Size" :inline t)
+               (cond
+                ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+                ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+                (t (format "%8d" (buffer-size))))))
+ ;; Explicitly require ibuffer-vc to get its column definitions, which
+ ;; can't be autoloaded
+ (with-eval-after-load 'ibuffer
+             (fullframe ibuffer ibuffer-quit))
+ (with-eval-after-load 'ibuffer
+             (require 'ibuffer-vc))
+ ;; Modify the default ibuffer-formats (toggle with `)
+ (setq ibuffer-formats
+       '((mark modified read-only vc-status-mini " "
+               (name 18 18 :left :elide)
+               " "
+               (size-h 9 -1 :right)
+               " "
+               (mode 16 16 :left :elide)
+               " "
+               filename-and-process)
+         (mark modified read-only vc-status-mini " "
+               (name 18 18 :left :elide)
+               " "
+               (size-h 9 -1 :right)
+               " "
+               (mode 16 16 :left :elide)
+               " "
+               (vc-status 16 16 :left)
+               " "
+               filename-and-process)))
+ (setq ibuffer-filter-group-name-face 'font-lock-doc-face)
+ (global-set-key (kbd "C-x C-b") 'ibuffer)
+ )
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (custom-set-variables
