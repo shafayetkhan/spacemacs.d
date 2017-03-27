b6fa0d9d816fe4beae8d07c8b2ec5815a8234aab
Shafayet Khan <shafayetkhan@gmail.com>
2017-03-27 16:58:13 -0600

    Try a different theme - darkmine

--- init.el
+++ init.el
@@ -151,6 +151,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(;;materialistic-seti
+                         darkmine
                          darkokai
                          monokai
                          material
